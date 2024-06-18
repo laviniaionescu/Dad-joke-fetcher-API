da0177d2587fa9fbee62c1d908ed3ce89a0b29b0
LaviniaIonescu <laviniaionescu9933@gmail.com>
2024-06-18 10:23:52 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+import requests
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+  return render_template('home.html')
+
+@app.route('/getjoke')
+def jokepage():
+    headers = {'Accept': 'application/json'}
+
+    response = requests.get('https://icanhazdadjoke.com/', headers=headers)
+    if response.status_code == 200:
+        joke = response.json()['joke']
+        return render_template('jokepage.html', joke=joke)
+    else:
+        return 'Error fetching joke from API'
+
+
+if __name__ == ('__main__'):
+  app.run(debug=True)
